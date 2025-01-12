9bfc314450540970785ac341a4db7a42f3917980
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2025-01-12 00:36:31 -0300

    Melhorias / ou não

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-// Script para abrir/fechar o menu hambúrguer
-document.getElementById("menu-toggle").addEventListener("click", function() {
-    var mobileMenu = document.getElementById("mobile-menu");
-    mobileMenu.classList.toggle("open"); // Alterna a classe 'open' para mostrar ou esconder o menu
-});
