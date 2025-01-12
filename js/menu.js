91de5868cd453c56270340b0de734b6d24e115a9
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2025-01-12 16:04:32 -0300

    Atualização

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navMobile = document.getElementById('nav-mobile');
+    navMobile.classList.toggle('active'); // Alterna a visibilidade do menu
+});
